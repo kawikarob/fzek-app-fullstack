68113ad4e60e59d689361970629b8afc669ea190
kawikarob <64801223+kawikarob@users.noreply.github.com>
2020-07-20 23:18:44 -0700

    add then and cath

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-require("dotenv").config();
-const mysql = require("mysql");
-var connection = mysql.createConnection({
-   host: process.env.RDS_HOST,
-   user: process.env.RDS_USER,
-   password: process.env.RDS_PASSWORD,
-   database: "fzek_app",
-});
+const express = require("express");
+const app = express();
 
-connection.connect();
+app.use("/api/v1/users", require("./api/v1/users"));
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      console.log(error);
-   }
-   console.log("The solution is: ", results[0].solution);
-});
-
-connection.end();
+const port = process.env.PORT || 3013;
+app.listen(port, () =>
+   console.log(`Server running at http://localhost:${port}`)
+);
---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+module.exports = {
+   toJson(data) {
+      return JSON.stringify(data);
+   },
+
+   toSafeParse(str) {
+      try {
+         JSON.parse(str);
+      } catch (err) {
+         console.log(err);
+         return str;
+      }
+      return JSON.parse(str); // could be undefined
+   },
+
+   //    toHash(password) {
+   //       const saltRounds = 12;
+   //       return bcrypt.hash(password, saltRounds);
+   //    },
+};
